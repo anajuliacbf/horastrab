e6009b8deb65c05ccb048acab8d43e3d2ed10d6d
Ana Julia Castelo Branco Fernandes <anajuliacbfernandes@gmail.com>
2023-04-20 09:21:14 -0300

    Create horastrab.py

---
+++ horastrab.py
@@ -0,0 +1,6 @@
+print ("Quantas horas você trabalha por dia?")
+hrsDia = int(input())
+print ("Quantos dias você trabalhou no mês?")
+diasMes = int(input())
+hrsMes = hrsDia * diasMes
+print ("O total de horas trabalhadas no mês é de", hrsMes)
